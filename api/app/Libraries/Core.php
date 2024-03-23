2ba5e7ec5b2c317f19314390ae1211b402237613
Chima Nwachukwu <tylerdgenius@Chimas-MBP.(none)>
2024-03-23 14:18:05 +0100

    feat: adding frontend views solution

--- api/app/Libraries/Core.php
+++ api/app/Libraries/Core.php
@@ -23,7 +23,7 @@ class Core{
         return $_SERVER['REQUEST_METHOD'];
     }
     
-    public function getUrl(){        
+    public function getUrl(){ 
         if(isset($_GET['url'])){
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
